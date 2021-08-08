4475dc85e2e6df25a859ec8ea1afb858c385774e
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-08-08 03:15:09 -0700

    Update countSort.cpp

--- countSort.cpp
+++ countSort.cpp
@@ -24,3 +24,34 @@ for (int i = n-1; i >=0; --i)
 for(int i=0;i<n;i++) 
    arr[i]=sortedArr[i];
 }
+
+
+
+// COUNT SORT USING MAX AND MIN BOTH TO REDUCE THE SPACE OF SORTING
+
+void countSort(vector<int> &arr){
+  int n = size(arr);
+  int BiggestElem = *max_element(arr.begin(),arr.end());
+  int smallestElem = *min_element(arr.begin(),arr.end());
+  int finalSize = BiggestElem-smallestElem+1;
+vector<int> hashArr(finalSize);
+for(auto &x:arr) {
+  int idx = x-smallestElem;
+  hashArr[idx]++;
+  }
+  //prefix sum of hashArr
+for (int i = 1; i <finalSize; ++i) 
+  hashArr[i]+=hashArr[i-1];
+  //filling of array in a sorted way
+vector<int> sortedArr(n);
+for (int i = n-1; i >=0; --i) {
+  int pos = hashArr[arr[i]-smallestElem]-1; //since the length of array is 10 then the index is 9 (last one)
+sortedArr[pos]=arr[i];
+hashArr[arr[i]-smallestElem]--;
+}
+//place all items from the sorted array into the given array
+for(int i=0;i<n;i++) 
+  arr[i]=sortedArr[i];
+}
+
+REFERENCE: https://youtu.be/p-OyKUgIrx4
