19499737dce83bf7af6a21cb03cb7861eaaf4885
KUNAL SINGH <76788100+singhkunal01@users.noreply.github.com>
2021-08-08 01:57:45 -0700

    Update countSort.cpp

--- countSort.cpp
+++ countSort.cpp
@@ -1,17 +1,26 @@
 
 // COUNT SORT: Time complexity of This Sort it O(n) where as Space Complexity is also O(n)
 
+//FIRST APPROACH WITHOUT USING MIN VALUE WE CAN TAKE THE SIZE OF BIGGEST ELEMENT IN THE WHOLE ARRAY
 void countSort(vector<int> &arr){
-	int n=size(arr);
-	int BiggestElem=*max_element(begEnd(arr));
-	vector<int> hashArr(BiggestElem+1);
-for(auto x:arr) hashArr[x]++; //to find the count of each element using hashmap
-vector<int> prefixArr(BiggestElem+1);
-for (int i = 1; i <=size(prefixArr); ++i) prefixArr[i]=prefixArr[i-1]+hashArr[i];
+int n = size(arr);
+int BiggestElem = *max_element(arr.begin(),arr.end());
+	
+//frequency of each value
+vector<int> hashArr(BiggestElem+1);
+for(auto &x:arr) 
+  hashArr[x]++;
+	
+//prefix sum of hashArr
+for (int i = 1; i <= BiggestElem; ++i) 
+   hashArr[i]+=hashArr[i-1];
+	
+//filling of array in a sorted way
 vector<int> sortedArr(n);
-for (int i = n-1; i >=0; --i) {
-sortedArr[--prefixArr[arr[i]]]=arr[i];
-}
-cout << "Sorted Array is: ";
-for(auto &val:sortedArr) cout << val<<sp;
+for (int i = n-1; i >=0; --i) 
+     sortedArr[--hashArr[arr[i]]]=arr[i];
+
+//place all items from the sorted array into the given array
+for(int i=0;i<n;i++) 
+   arr[i]=sortedArr[i];
 }
